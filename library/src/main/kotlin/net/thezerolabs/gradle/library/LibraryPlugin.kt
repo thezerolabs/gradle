3f2f6c903a8da28abf7a71d599e317d24fffdbd2
Suresh Reddy G <sureshreddy.guntaka@gmail.com>
2025-09-21 14:27:26 +0530

    refactor: simplify GitHub packages publishing configuration using Action interfaces

--- library/src/main/kotlin/net/thezerolabs/gradle/library/LibraryPlugin.kt
+++ library/src/main/kotlin/net/thezerolabs/gradle/library/LibraryPlugin.kt
@@ -8,6 +8,10 @@ import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.Action
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.api.artifacts.repositories.PasswordCredentials
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import javax.inject.Inject
 
@@ -173,18 +177,16 @@ class LibraryPlugin : Plugin<Project> {
                     ?: env("GITHUB_TOKEN")
 
                 if (!url.isNullOrBlank() && !user.isNullOrBlank() && !token.isNullOrBlank()) {
-                    project.extensions.configure(PublishingExtension::class.java) { pub ->
-                        pub.repositories { repos ->
-                            repos.maven { m ->
-                                m.name = "GitHubPackages"
-                                m.setUrl(url)
-                                m.credentials { c ->
-                                    c.username = user
-                                    c.password = token
-                                }
-                            }
-                        }
-                    }
+                    val pub = project.extensions.getByType(PublishingExtension::class.java)
+                    pub.repositories(Action<RepositoryHandler> { repos ->
+                        repos.maven(Action<MavenArtifactRepository> { m ->
+                            m.setUrl(project.uri(url))
+                            m.credentials(Action<PasswordCredentials> { c ->
+                                c.username = user
+                                c.password = token
+                            })
+                        })
+                    })
                 } else {
                     project.logger.info("[library] Skipping GitHub Packages publishing repository: missing url/user/token. url=$url user=${user?.let { "***" }} token=${token?.let { "***" }}")
                 }
